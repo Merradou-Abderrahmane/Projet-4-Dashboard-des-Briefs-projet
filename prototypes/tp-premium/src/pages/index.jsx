9338dbe38115398aa3f05ea8f78232a4ca909348
Merradou-Abderrahmane <92019711+Merradou-Abderrahmane@users.noreply.github.com>
2022-12-12 16:57:08 +0100

    save

---
+++ prototypes/tp-premium/src/pages/index.jsx
@@ -0,0 +1,13 @@
+import axios from "axios";
+import react from "react";
+
+class index extends react.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: [],
+            task: "",
+            id: ""
+        };
+}
+}
