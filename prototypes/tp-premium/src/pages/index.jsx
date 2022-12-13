753f5eb8fdf2276302e84c213513278cd8448e19
Merradou-Abderrahmane <92019711+Merradou-Abderrahmane@users.noreply.github.com>
2022-12-13 07:17:24 +0100

    Crud done

--- prototypes/tp-premium/src/pages/index.jsx
+++ prototypes/tp-premium/src/pages/index.jsx
@@ -10,4 +10,47 @@ class index extends react.Component {
             id: ""
         };
 }
+    componentDidMount() {
+        axios.get("http://localhost:3000/api/tasks").then((res) => {
+            this.setState({ data: res.data });
+        });
+    }
+
+    handleChange = (e) => {
+        this.setState({ task: e.target.value });
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        axios.post("http://localhost:3000/api/tasks", {
+            task: this.state.task
+        }).then((res) => {
+            this.setState({ data: res.data });
+        });
+    }
+
+    handleDelete = (id) => {
+        axios.delete(`http://localhost:3000/api/tasks/${id}`).then((res) => {
+            this.setState({ data: res.data });
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text" onChange={this.handleChange} />
+                    <button type="submit">Add</button>
+                </form>
+                <ul>
+                    {this.state.data.map((item) => (
+                        <li key={item.id}>
+                            {item.task}
+                            <button onClick={() => this.handleDelete(item.id)}>Delete</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        );
+    }
 }
