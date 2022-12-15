c5754d21eb45ca90f9e589d14484c3194f6541f0
Merradou-Abderrahmane <92019711+Merradou-Abderrahmane@users.noreply.github.com>
2022-12-15 10:13:35 +0100

    fix migration problem

--- realisation/database/migrations/2022_12_15_043525_create_groupes_table.php
+++ realisation/database/migrations/2022_12_15_043525_create_groupes_table.php
@@ -13,15 +13,27 @@ return new class extends Migration
      */
     public function up()
     {
+        if (!Schema::hasTable( table :'groupes'))  {
         Schema::create('groupes', function (Blueprint $table) {
             $table->id();
             $table->string('nom_groupe');
-            $table->unsignedBigInteger('annee_formation_id');
-            $table->foreign('annee_formation_id')->references('id')->on('anne_formations');
             $table->string('logo');
             $table->timestamps();
         });
     }
+        //create new with foreign keys above
+        Schema::table('groupes', function (Blueprint $table) {
+            $table->unsignedBigInteger('annee_formation_id');
+            $table->foreign('annee_formation_id')->references('id')->on('annes_formation');
+            $table->unsignedBigInteger('formateur_id');
+            $table->foreign('formateur_id')->references('id')->on('formateurs');
+            });
+
+        
+        
+
+}
+        
 
     /**
      * Reverse the migrations.
